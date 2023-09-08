c199106fed7c6430ec1316272a21cc2cbab544fe
Narendra Dukhande <140737376+narendra3003@users.noreply.github.com>
2023-09-08 09:55:36 +0530

    Completed 16_14_narendra.c

---
+++ 16_14_narendra.c
@@ -0,0 +1,157 @@
+/*Array implementation of double-ended Queue*/
+
+/*
+name = Narendra Dukhande
+Roll No. = 16
+pid = 14
+SE-IT (sem 3)
+2023-24
+*/
+
+#include<stdio.h>
+int f=-1, r=-1;
+int n=5, x, choice;
+int arr[5];
+void makeEmpty(int arr[])
+{
+    //making Queue empty
+    f=r=-1;
+}
+void enQueueRear(int x, int arr[])
+{
+    if((r==n-1)&&(f==0)||(f==r+1))
+    {
+        //Queue Overflow
+        printf("Queue is full");
+        return;
+    }
+    if(f==-1)
+    {
+        //entering first value
+        f++;
+    }
+    r = (r+1)%n;
+    arr[r]= x;
+}
+void enQueueFront(int x, int arr[])
+{
+    if((r==n-1)&&(f==0)||(f==r+1))
+    {
+        //Queue Overflow
+        printf("Queue is full");
+        return;
+    }
+    if(f==-1)
+    {
+        //entering first value
+        enQueueRear(x, arr);
+        return;
+    }
+    if(f==0){
+    	//entering element in end as cannot enter before
+    	f=n-1;
+    	arr[f]=x;
+    	return;
+    }
+    f--;
+    arr[f]=x;
+}
+void deQueueFront(int arr[])
+{
+    if(f==-1)
+    {
+        //Queue Underflow
+        printf("Queue is empty");
+        return;
+    }
+    if(f==r)
+    {
+        //single element to pop
+        f=-1;r=-1;
+        return;
+    }
+    f=(f+1)%n;
+}
+void deQueueRear(int arr[])
+{
+    if(f==-1)
+    {
+        //Queue Underflow
+        printf("Queue is empty");
+        return;
+    }
+    if(f==r)
+    {
+        //single element to pop
+        f=-1;r=-1;
+        return;
+    }
+    if(r==0){
+    	r=n-1;
+    }
+    else{
+    	r--;
+    }
+}
+void printArr(int arr[])
+{
+    if(f==-1)
+    {
+        //no element to print
+        printf("Queue is empty");
+        return;
+    }
+	int i=f-1;
+    do
+    {
+    	i=(i+1)%n;
+        printf("%d\n", arr[i]);
+    }while(i!=r);
+}
+void main()
+{
+
+    /*calling the everlasting choices*/
+    while(choice!=6)
+    {
+        /*description of menu bar*/
+        printf("\n\n\n\n-----These are the menu panel-----");
+        printf("\n1. Enter in Queue from front end");
+        printf("\n2. Enter in Queue from rear end");
+        printf("\n3. Delete from Queue from front end");
+        printf("\n4. Delete from Queue from rear end");
+        printf("\n5. Traverse");
+        printf("\n6. Exit");
+        printf("\nenter the option no.: ");
+        scanf("%d", &choice);
+
+        /*switch on entered choice*/
+        switch(choice)
+        {
+            /*case-wise calling functions*/
+            case 1: 
+            printf("enter the number you want to push: ");
+            scanf("%d", &x);
+            enQueueFront(x, arr);
+                break;
+            case 2: 
+            printf("enter the number you want to push: ");
+            scanf("%d", &x);
+            enQueueRear(x, arr);
+                break;
+            case 3: deQueueFront(arr);
+                break;
+            case 4: deQueueRear(arr);
+                break;
+            case 5: printArr(arr);
+                break;
+            case 6:
+                break;
+            default:
+                printf("incorrect choice");
+        }
+    }/*end of while*/
+
+    /*code execution completed*/
+    printf("You got exit\n\n");
+}
